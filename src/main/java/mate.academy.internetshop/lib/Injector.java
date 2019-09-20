ecabb235691e0d2c9f22440f5b32eea5578b4360
RomanMinevich <52323566+RomanMinevich@users.noreply.github.com>
2019-09-20 13:29:41 +0300

    Update Injector.java

--- src/main/java/mate.academy.internetshop/lib/Injector.java
+++ src/main/java/mate.academy.internetshop/lib/Injector.java
@@ -1,79 +1,77 @@
 package mate.academy.internetshop.lib;
 
-import java.io.File;
-import java.io.IOException;
+import static java.util.stream.Stream.of;
+
 import java.lang.reflect.Field;
-import java.net.URL;
-import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
 import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import mate.academy.internetshop.Factory;
+import mate.academy.internetshop.Main;
+import mate.academy.internetshop.dao.impl.BucketDaoImpl;
+import mate.academy.internetshop.dao.impl.ItemDaoImpl;
+import mate.academy.internetshop.dao.impl.OrderDaoImpl;
+import mate.academy.internetshop.dao.impl.UserDaoImpl;
+import mate.academy.internetshop.service.impl.BucketServiceImpl;
+import mate.academy.internetshop.service.impl.ItemServiceImpl;
+import mate.academy.internetshop.service.impl.OrderServiceImpl;
+import mate.academy.internetshop.service.impl.UserServiceImpl;
 
 public class Injector {
-    private static final String PROJECT_MAIN_PACKAGE = "mate.academy.internetshop";
-    private static final List<Class> classes = new ArrayList<>();
-
-    static {
-        try {
-            classes.addAll(getClasses(PROJECT_MAIN_PACKAGE));
-        } catch (ClassNotFoundException | IOException exception) {
-            exception.printStackTrace();
-        }
-    }
 
     public static void injectDependency() throws IllegalAccessException {
-        List<Field> annotatedFields = classes
-                    .stream()
-                    .map(Class::getDeclaredFields)
-                    .flatMap(Stream::of)
-                    .filter(field -> field.isAnnotationPresent(Inject.class))
-                    .collect(Collectors.toList());
-        for (Field field : annotatedFields) {
-            Object object = AnnotatedClassMap.implement(field.getType());
-            if (object.getClass().isAnnotationPresent(Service.class)
-                    || object.getClass().isAnnotationPresent(Dao.class)) {
+        injectToMain();
+        injectToService(ItemServiceImpl.class);
+        injectToService(BucketServiceImpl.class);
+        injectToService(OrderServiceImpl.class);
+        injectToService(UserServiceImpl.class);
+    }
+
+    private static void injectToMain() throws IllegalAccessException {
+        for (Field field : getAnnotatedFields(Main.class)) {
+            if (checkDependency(field, ItemServiceImpl.class)) {
                 field.setAccessible(true);
-                field.set(null, object);
+                field.set(null, Factory.getItemService());
+            } else if (checkDependency(field, BucketServiceImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getBucketService());
+            } else if (checkDependency(field, OrderServiceImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getOrderService());
+            } else if (checkDependency(field, UserServiceImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getUserService());
             }
         }
     }
 
-    private static List<Class> getClasses(String packageName)
-            throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-        List<File> dirs = new ArrayList<File>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-        ArrayList<Class> classes = new ArrayList<Class>();
-        for (File directory : dirs) {
-            classes.addAll(findClasses(directory, packageName));
-        }
-        return classes;
-    }
-
-    private static List<Class> findClasses(File directory, String packageName)
-            throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<Class>();
-        if (!directory.exists()) {
-            return classes;
-        }
-        File[] files = directory.listFiles();
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class")) {
-                classes.add(
-                        Class.forName(packageName + '.' + file.getName().substring(
-                        0, file.getName().length() - 6)));
+    private static void injectToService(Class service) throws IllegalAccessException {
+        for (Field field : getAnnotatedFields(service)) {
+            if (checkDependency(field, ItemDaoImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getItemDao());
+            } else if (checkDependency(field, BucketDaoImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getBucketDao());
+            } else if (checkDependency(field, OrderDaoImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getOrderDao());
+            } else if (checkDependency(field, UserDaoImpl.class)) {
+                field.setAccessible(true);
+                field.set(null, Factory.getUserDao());
             }
         }
-        return classes;
+    }
+
+    private static List<Field> getAnnotatedFields(Class dependant) {
+        return of(dependant.getDeclaredFields())
+                .filter(field -> field.isAnnotationPresent(Inject.class))
+                .collect(Collectors.toList());
+    }
+
+    private static boolean checkDependency(Field annotatedField, Class dependency) {
+        return of(dependency.getInterfaces())
+                .anyMatch(dependencyInt -> dependencyInt.equals(annotatedField.getType()))
+                && (dependency.isAnnotationPresent(Service.class)
+                || dependency.isAnnotationPresent(Dao.class));
     }
 }
